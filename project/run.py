c7cc948bd4b09e26df9ccf75c9f7bd79ac5014a3
Frank Chen <frankchen1698@gmail.com>
2017-04-11 09:32:35 -0700

    Render dicom video via pylab
    * Write description about dicom files
    * Fix linking in main page

---
+++ project/run.py
@@ -0,0 +1,11 @@
+import dicom
+import pylab
+
+ds = dicom.read_file("2d_angiogram.dcm")
+
+frames = len(ds.pixel_array[:,1,1])
+for i in xrange(frames):
+	pixel_array_2d = ds.pixel_array[i,:,:]
+	pylab.imshow(pixel_array_2d, cmap=pylab.cm.bone)
+	pylab.pause(.1)
+	pylab.draw()
