41d54f7273d01a74a9f737169177b18c5ac730bf
Arvin Nguyen <arvinnguyen@ucla.edu>
2017-04-11 09:59:26 -0700

    Simply for loop

--- project/run.py
+++ project/run.py
@@ -5,9 +5,7 @@ import pylab
 
 ds = dicom.read_file("2d_angiogram.dcm")
 
-frames = len(ds.pixel_array[:,1,1])
-for i in xrange(frames):
-	pixel_array_2d = ds.pixel_array[i,:,:]
-	pylab.imshow(pixel_array_2d, cmap=pylab.cm.bone)
+for frame in ds.pixel_array:
+	pylab.imshow(frame, cmap=pylab.cm.bone)
 	pylab.pause(.1)
 	pylab.draw()
