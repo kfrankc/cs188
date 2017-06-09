a913178d08bcc3a9e6838ca6df0fd02213dc3246
Arvin Nguyen <arvinnguyen@ucla.edu>
2017-06-09 00:47:42 -0700

    Fix component mount callback func

--- animation/py/main.py
+++ animation/py/main.py
@@ -15,19 +15,32 @@ def transpose_x_y(point):
 def transpose_points(path_array):
     return map(transpose_x_y, path_array)
 
+def create_vector(x,y):
+    return {
+        "x": x,
+        "y": y,
+        }
+
 # Input: array of points
-# output: array of vectors to the next point
+# output: array of vectors to the next point # TODO
 def get_vectors(path_array):
     vectors = []
     for i in range(len(path_array)):
         if i == len(path_array) - 1: # check for last pixel
-            vectors.append((0,0))     # segment ended. vector to nowhere...
+            #vectors.append((0,0))     # segment ended. vector to nowhere...
+
+            vectors.append({
+                "x": 0,
+                "y": 0,
+            })
+
         else:
             x1, y1 = path_array[i]
             x2, y2 = path_array[i+1]
-            vectors.append((x2-x1, y2-y1))
-    return vectors
+            vectors.append(create_vector(x2-x1, y2-y1))
 
+            #vectors.append((x2-x1, y2-y1))   # tuple/array representation
+    return vectors
 
 with open('path.json') as f:
     path_array = json.load(f)
@@ -41,16 +54,16 @@ with open('path.json') as f:
     for x in range(width):
         vec_field.append([])
         for y in range(height):
-            vec_field[x].append((0,0))
+            vec_field[x].append(create_vector(0,0))
 
     for i, (x, y) in enumerate(path_array):
-        vec_field[x][y] = vec_field[i]
+        vec_field[x][y] = vectors[i]
 
-    print(len(vec_field[89]))
-    #print(vec_field)
-
-
-
+    #print(json.dumps(vectors, indent=2))
+    #print(vec_field[357][1022])
+    print(json.dumps(vec_field, indent=2))
 
 
-    #pprint(get_vectors(path_array))
+    # Valid
+    #print(json.dumps(path_array, indent=2))
+
--- animation/src/App.js
+++ animation/src/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import angiogram from './angiogram.js';
+import axios from 'axios';
 import "./angiogram.css";
 
-class AppContainer extends Component {
+const colors = [
+  [255,255,0],
+  [255,226,0],
+  [255,198,0],
+  [255,170,0],
+  [255,141,0],
+  [255,113,0],
+  [255,85,0],
+  [255,56,0],
+  [255,28,0],
+  [255,0,0],
+]
 
-  componentDidMount() {
+
+const f = () => {
     let canvas = document.getElementById('canvas');
     let ctx = canvas.getContext('2d');
     if (!canvas.getContext) {
         // canvas-unsupported code here
     }
-    const colors = [
-        [255,255,0],
-        [255,226,0],
-        [255,198,0],
-        [255,170,0],
-        [255,141,0],
-        [255,113,0],
-        [255,85,0],
-        [255,56,0],
-        [255,28,0],
-        [255,0,0],
-    ]
     const vector_field = [];
     for(let x = 0; x < canvas.width; x++) {
         vector_field.push([]);
@@ -30,27 +31,86 @@ class AppContainer extends Component {
             vector_field[x].push({ x: 1, y: 1 })
         }
     }
-
     ctx.fillStyle = 'rgb(200, 0, 0)';
     ctx.fillRect(10, 10, 50, 50);
     ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
     ctx.fillRect(30, 30, 50, 50);
-
     angiogram(vector_field, canvas, colors);
+}
+
+
+class AppContainer extends Component {
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dimensions: null,
+    };
+  }
+
+  componentDidMount() {
+
+    this.setState({
+      f: f,
+      dimensions: {
+        height: 150,
+        width: 150,
+      }
+    })
+
+    /*
+    const component = this;
+    axios.get('/field.json') // todo: public folder node env
+      .then((response) => {
+        const v_field = response.data;
+        const width = v_field[0];
+        const height = v_field[1];
+        debugger
+        component.setState({
+          dimensions: {
+            width: width.length,
+            height: height.length,
+          }
+        })
+        console.log(v_field[0][0]);
+        angiogram(v_field, canvas, colors);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    */
   }
 
   render() {
+    // TODO: what is default?  need canvas rendered in order to get context
+    if (this.state.dimensions === null) {
+      return null;
+      /*
+      return (
+        <App
+          width={150}
+          height={150}
+        />)
+      */
+    }
+
     return (
       <App
-        width={150}
-        height={150}
+        width={this.state.dimensions.width}
+        height={this.state.dimensions.height}
+        angiogram={this.state.f}
       />
     );
   }
 }
 
 class App extends Component {
-    render() {
+  componentDidMount() {
+    this.props.angiogram();
+  }
+
+  render() {
     let style = {
       width: this.props.width,
       height: this.props.height,
@@ -68,6 +128,7 @@ class App extends Component {
 }
 
 App.propTypes = {
+  angiogram: PropTypes.func.isRequired,
   width: PropTypes.number.isRequired,
   height: PropTypes.number.isRequired,
 };
